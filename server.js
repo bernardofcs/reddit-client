6ad6057dda3adcb2d6c71f4751471ba2961e6c58
bernardofcs <bernardofcs@hotmail.com>
2017-04-15 13:45:55 -0400

    initial server changes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
 app.set('port', (process.env.PORT || 3001));
 
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 });
+
+app.get('/subreddits', (req, res) => {
+
+})
